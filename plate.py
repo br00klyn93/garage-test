70526622ab26c3495893b307118d7834897634bb
br00klyn93 <bmclaury93@gmail.com>
2019-01-15 13:31:08 -0800

    Update plate.py

--- plate.py
+++ plate.py
@@ -7,6 +7,16 @@ import sys
 import base64
 import json
 import os
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+
 
 
 state = ""
@@ -106,7 +116,7 @@ def get_plate():
     
 
 
-def main():
-    get_plate()
-
-main()
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
