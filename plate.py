e33b02406cc471e3f3aa3b60df43652f742a8ef5
br00klyn93 <bmclaury93@gmail.com>
2019-01-15 13:34:14 -0800

    Update plate.py

--- plate.py
+++ plate.py
@@ -13,8 +13,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def hello(inf):
+    return inf
 
 
 
@@ -62,6 +62,7 @@ def get_msrp():
 
     for i in info:
         print(i)
+        hello(i)
 
     cars = sheet.findall(info[1])
     
@@ -120,3 +121,4 @@ if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+    get_model()
